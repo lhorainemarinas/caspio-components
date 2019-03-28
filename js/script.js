71da1138e6c499bd0c2f994fb832cc5ddb290cbd
Lhoraine Fatima Mariñas <lhoraine.marinas@caspio.com>
2019-03-28 11:26:42 +0800

    03/28/2019

--- js/script.js
+++ js/script.js
@@ -639,7 +639,7 @@ jQuery(function() {
 			subpage = $('.submenu-left .has-child ul li a');
 		
 		if(subpage.is('[href^="page"]')) {
-			console.log($(this).length)
+			console.log('a' + $(this).length)
 			if(homeUrl && (url != github)) {
 				subpage.each(function(){
 					var pname = $(this)[0].pathname,
@@ -662,7 +662,7 @@ jQuery(function() {
 				});
 			}
 		} else if(!subpage.is('[href^="page"]')) {
-			console.log($(this).length)
+			console.log('b' + $(this).length)
 			subpage.each(function(){
 				var pname = $(this)[0].pathname,
 					arr_urls = pname.split('/'),
